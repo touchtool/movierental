cde2fc86f0c03db55c0d683c0727c1b7aea9b705
Kittison <kittison.j@ku.th>
2021-10-13 22:34:26 +0700

    finish task 3

--- customer.py
+++ customer.py
@@ -28,7 +28,7 @@ class Customer:
             Returns:
                 the statement as a String
         """
-        total_amount = 0 # total charges
+        total_amount = 0  # total charges
         frequent_renter_points = 0 
         statement = f"Rental Report for {self.name}\n\n"
         fmt = "{:32s}    {:4s} {:6s}\n"
@@ -36,13 +36,12 @@ class Customer:
         fmt = "{:32s}   {:4d} {:6.2f}\n"
 
         for rental in self.rentals:
-            amount = rental.rental_price()
             # award renter points
             frequent_renter_points = rental.renter_points(frequent_renter_points)
             #  add detail line to statement
-            statement += fmt.format(rental.get_movie().get_title(), rental.get_days_rented(), amount)
+            statement += fmt.format(rental.get_movie().get_title(), rental.get_days_rented(), rental.rental_price())
             # and accumulate activity
-            total_amount += amount
+            total_amount += rental.rental_price()
 
         # footer: summary of charges
         statement += "\n"
