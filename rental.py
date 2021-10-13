39008796c839e099e7d3e75ebf9a385e67def84d
Kittison <kittison.j@ku.th>
2021-10-13 22:17:46 +0700

    finish task 2

--- rental.py
+++ rental.py
@@ -1,3 +1,8 @@
+import logging
+
+from movie import Movie
+
+
 class Rental:
 	"""
 	A rental of a movie by customer.
@@ -22,3 +27,31 @@ class Rental:
 
 	def get_days_rented(self):
 		return self.days_rented
+
+	def rental_price(self):
+		# compute rental change
+		amount = 0
+		if self.get_movie().get_price_code() == Movie.REGULAR:
+			# Two days for $2, additional days 1.50 each.
+			amount = 2.0
+			if self.get_days_rented() > 2:
+				amount += 1.5 * (self.get_days_rented() - 2)
+		elif self.get_movie().get_price_code() == Movie.CHILDREN:
+			# Three days for $1.50, additional days 1.50 each.
+			amount = 1.5
+			if self.get_days_rented() > 3:
+				amount += 1.5 * (self.get_days_rented() - 3)
+		elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+			# Straight per day charge
+			amount = 3 * self.get_days_rented()
+		else:
+			log = logging.getLogger()
+			log.error(f"Movie {self.get_movie()} has unrecognized priceCode {self.get_movie().get_price_code()}")
+		return amount
+
+	def renter_points(self, frequent_renter_points):
+		if self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+			frequent_renter_points += self.get_days_rented()
+		else:
+			frequent_renter_points += 1
+		return frequent_renter_points
